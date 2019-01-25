c0becbd37c6d86596d0e96e703e734bed9ad31cb
Nikhil <nikhil.badwaik@nike.com>
2019-01-25 09:25:57 -0800

    Youtube API Key

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React, {PureComponent} from 'react'
 import SearchField from './SearchField';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyC0nBySZQaiRMzCXOBqRdAVx0HRgFT5YdY';
 
 const HelloMessage = ({ name }) => {
  return <div id="foo">hello again {name}</div>;
@@ -7,21 +10,32 @@ const HelloMessage = ({ name }) => {
 
 function onButtonClick(){
   console.log("are you really coming here");
+
   document.getElementById('textInput').className="show";
 }
 const displayNone = {display: "none"};
 
+
 export default class App extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      YTList: [],
+    }
+  }
   render(){
     return (
       <div>
         <HelloMessage name="nikhil"/>
         <SearchField id="foo" onTextDone={(value) => {
-          // <HelloMessage name="nikhil"/>
-          document.getElementById('submitted').style.display="block";
-          console.log('>>> log: ', value);
+          // document.getElementById('submitted').style.display="block";
+          YTSearch({key: API_KEY, term: value}, (data) => {
+            this.setState({YTList: data});
+          });
         }}/>
-        <div id= "submitted" style={displayNone}>submitted successfully</div>
+        {this.state.YTList.map((item) => {
+           return (<iframe width="420" height="345" src= {'https://www.youtube.com/embed/'+ item.id.videoId}></iframe>);
+        })}
       </div>
     );
   }
--- SearchField.js
+++ SearchField.js
@@ -6,20 +6,22 @@ window.doThing = () => {
 export default class SearchField extends PureComponent {
   constructor(){
     super();
-
-    // this.onKey/ = this.onKey.bind(this);
     this.submitFunc = this.submitFunc.bind(this)
     this.input = React.createRef()
+    this.state = {
+      firstName: 'nikhil'
+    }
   }
-  
+
   submitFunc(event) {
     event.preventDefault();
     this.props.onTextDone(this.input.current.value);
+    // this.setState({firstName: 'Riaan'});
   }
   render(){
     return (
       <form onSubmit={this.submitFunc}>
-        <input ref={this.input} type="text"/>
+        <input ref={this.input} type="text" />
         <button> submit</button>
       </form>
     );
