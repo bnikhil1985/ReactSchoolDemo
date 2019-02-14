fd02e9116c313033df32bddffb8b8d30ef2839de
Nikhil <nikhil.badwaik@nike.com>
2019-02-14 09:12:48 -0800

    updated

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import React, {PureComponent} from 'react'
-import SearchField from './SearchField';
+import React, {PureComponent} from 'react';
+import { createStore } from 'redux';
 import YTSearch from 'youtube-api-search';
+import SearchField from './SearchField';
+import Video from './Video';
 
 const API_KEY = 'AIzaSyC0nBySZQaiRMzCXOBqRdAVx0HRgFT5YdY';
+
+const reducer = (state, action) => {
+  console.log('I am in the reducer');
+  return {storeData: action.data};
+}
+const store = createStore(reducer);
+store.dispatch({
+  type: '123',
+  data: 'abc'
+})
+console.log('Reducer :----', store.getState());
+
 
 const HelloMessage = ({ name }) => {
  return <div id="foo">hello again {name}</div>;
@@ -17,24 +31,27 @@ const displayNone = {display: "none"};
 
 
 export default class App extends PureComponent {
-  constructor() {
-    super();
-    this.state = {
-      YTList: [],
-    }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     YTList: [],
+  //   }
+  //   this.YTSearch = this.YTSearch.bind(this)
+  // }
+  state = { YTList: [] }
+  YTSearch = (value) => {
+    // document.getElementById('submitted').style.display="block";
+    YTSearch({key: API_KEY, term: value}, (data) => {
+      this.setState({YTList: data});
+    });
   }
   render(){
     return (
       <div>
         <HelloMessage name="nikhil"/>
-        <SearchField id="foo" onTextDone={(value) => {
-          // document.getElementById('submitted').style.display="block";
-          YTSearch({key: API_KEY, term: value}, (data) => {
-            this.setState({YTList: data});
-          });
-        }}/>
+        <SearchField id="foo" onTextDone={this.YTSearch}/>
         {this.state.YTList.map((item) => {
-           return (<iframe width="420" height="345" src= {'https://www.youtube.com/embed/'+ item.id.videoId}></iframe>);
+           return <Video key ={item.id.videoId} videoId={item.id.videoId}/>;
         })}
       </div>
     );
